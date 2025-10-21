4ceafcccca37b6a5ba18db954f9b2c60fa488ba8
CMDESIGN8 <cm@lowcargo.lat>
2025-10-21 10:01:08 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,32 @@
 const API_URL = "https://lupiback.onrender.com"; // URL del backend desplegado
 
+export const getBots = async () => {
+  const response = await fetch(API_URL);
+  if (!response.ok) throw new Error("Error al cargar los bots");
+  const data = await response.json();
+  return data.bots;
+};
+
+export const startMatch = async (characterId, botId) => {
+  const response = await fetch(`${API_URL}/match`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ characterId, botId }),
+  });
+  if (!response.ok) throw new Error("Error al iniciar la partida");
+  return await response.json();
+};
+
+export const finishMatch = async (matchId, finalScore) => {
+  const response = await fetch(`${API_URL}/${matchId}/finish`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(finalScore), // { player1Score, player2Score }
+  });
+  if (!response.ok) throw new Error("Error al finalizar la partida");
+  return await response.json();
+};
+
 export async function getProfile(userId) {
   const res = await fetch(`${API_URL}/profiles?id=${userId}`);
   return res.json();
